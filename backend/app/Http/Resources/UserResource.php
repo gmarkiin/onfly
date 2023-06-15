e3ce1a0087f18c81a237c63fce5eb962900b1ef7
Marcos G Miranda <gmarcos38@gmail.com>
2023-06-15 20:57:02 -0300

    featue: add login

---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return $this->buildReturn($this->resource);
+    }
+
+    private function buildReturn(User $user): array
+    {
+        $newlyCreatedReturn = [
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'date_created' => $user->created_at,
+            ],
+            'token' => $user->token
+        ];
+
+        return match ($user->stage) {
+            User::STAGE_NEWLY_CREATED => $newlyCreatedReturn
+        };
+    }
+}
