8dfa11f68e7ee1f8429a004ba5aaddce4b7db0c9
Marcos G Miranda <gmarcos38@gmail.com>
2023-06-15 20:56:30 -0300

    featue: add login

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterPostRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterPostRequest $request): UserResource
+    {
+        $user = new User();
+        $user->createUser($request->toArray());
+
+        return new UserResource($user);
+    }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use AllowDynamicProperties;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
+#[AllowDynamicProperties]
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+    public const STAGE_NEWLY_CREATED = 'newly_created';
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +47,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function createUser(array $data): void
+    {
+        self::fill([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ])->save();
+        $this->token = $this->createToken('apiAuthToken')->plainTextToken;
+        $this->stage = self::STAGE_NEWLY_CREATED;
+    }
 }
